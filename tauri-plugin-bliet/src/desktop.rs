fa160af80c187ba109571427f491365c6b46b29b
Pascal <pascal@mac.speedport.ip>
2025-12-10 21:08:02 +0100

    mal angefangen mit der Desktop implementierung des plugins

--- tauri-plugin-bliet/src/desktop.rs
+++ tauri-plugin-bliet/src/desktop.rs
@@ -13,10 +13,23 @@ pub fn init<R: Runtime, C: DeserializeOwned>(
 /// Access to the bliet APIs.
 pub struct Bliet<R: Runtime>(AppHandle<R>);
 
+use tauri::api::dialog::FileDialogBuilder;
+
 impl<R: Runtime> Bliet<R> {
-  pub fn ping(&self, payload: PingRequest) -> crate::Result<PingResponse> {
-    Ok(PingResponse {
-      value: payload.value,
-    })
-  }
+    pub fn pick_image(&self) -> crate::Result<String> {
+        let (tx, rx) = std::sync::mpsc::channel();
+
+        FileDialogBuilder::new()
+            .add_filter("Images", &["png", "jpg", "jpeg"])
+            .pick_file(move |file_path| {
+                let _ = tx.send(
+                    file_path
+                        .map(|p| p.to_string_lossy().to_string())
+                        .unwrap_or_default(),
+                );
+            });
+
+        let result = rx.recv().unwrap_or_default();
+        Ok(result)
+    }
 }
